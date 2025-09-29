71d6807009aa1d2bc175d6c5e6be08024ef247ee
Appr0ximity <hvshekharmurthy@gmail.com>
2025-09-29 22:57:42 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/crypto-wallet"
 })
